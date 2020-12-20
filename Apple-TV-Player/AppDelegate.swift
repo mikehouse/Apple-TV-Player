a84aa73a4e92a6a0f2bf58283332fd802bd5ab05
Mikhail Demidov <mike.house.nsk@gmail.com>
2020-12-20 16:45:23 +0700

    remove test playlist file from app bundle

--- Apple-TV-Player/AppDelegate.swift
+++ Apple-TV-Player/AppDelegate.swift
@@ -15,17 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        /*#if DEBUG
-            let fileManager = FileSystemManager()
-            if let playlist = Bundle.main.url(forResource: "plst", withExtension: "m3u") {
-                do {
-                    try fileManager.download(file: playlist, name: "2090000.ru")
-                } catch {
-                    os_log(.error, "playlist download error: %s", String(describing: error))
-                }
-            }
-        #endif*/
-         print(FileManager.default.temporaryDirectory.deletingLastPathComponent())
         return true
     }
 }
